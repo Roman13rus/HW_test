445e4ae8c8bfcd41085f543c3baf98c1091ce498
Roman Rogschev <omuch13rus@rambler.ru>
2024-02-04 21:54:31 +0200

    initional commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+export default function health(data) {
+  if (data.health > 50) {
+    return 'healthy';
+  } else if (data.health <= 50 && data.health >= 15) {
+    return 'wounded';
+  }
+  return 'critical';
+}
