b2c13a05711f7d3d3a1671d5520043a98adc5d87
Roman Rogschev <omuch13rus@rambler.ru>
2024-02-16 23:45:38 +0200

    jestglobals

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-// export default function health(data) {
-//   if (data.health > 50) {
-//     return 'healthy';
-//   } else if (data.health <= 50 && data.health >= 15) {
-//     return 'wounded';
-//   }
-//   return 'critical';
-// }
 export default function health(data) {
   if (data.health > 50) {
     return 'healthy';
